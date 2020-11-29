4407a81dd4b96bb80e9c0f3a84fa756c96a4c34b
Ivan Krivyakov <ivan@ikriv.com>
2020-11-28 23:57:39 -0500

    A memoizing version that keeps the tally of factors

--- PolyaConjecture.cpp
+++ PolyaConjecture.cpp
@@ -7,68 +7,72 @@
 
 using namespace std;
 
+const int TOTAL_NUMBERS = 1000 * 1000 * 1000; // 1 billion
+const int MAX_PRIME_TO_ADD = int(sqrt(TOTAL_NUMBERS)+1e-5);
+
 vector<int> primes;
+vector<short> num_factors = { 0 };
+int even_factors = 1; // total numbers with even number of factors seen so far
 
-static int get_num_prime_factors(int n, int ceil_sqrt_n)
+static short calc_num_prime_factors(int n, int max_prime_idx_to_consider)
 {
-    int factors = 0;
-    for (auto prime: primes)
+    assert((int)num_factors.size() == n - 1);
+    for (int prime_idx = 0; prime_idx<=max_prime_idx_to_consider; ++prime_idx)
     {
-        if (prime > n || prime > ceil_sqrt_n) break;
-
-        while (n % prime == 0)
+        auto prime = primes[prime_idx];
+        if (n % prime == 0)
         {
-            n /= prime;
-            ++factors;
+            auto factors = num_factors[n / prime - 1] + 1;
+            num_factors.push_back(factors);
+            if (factors % 2 == 0) ++even_factors;
+            return factors;
         }
     }
 
-    if (!factors)
-    {
-        primes.push_back(n);
-        return 1;
-    }
-
-    if (n != 1)
-    {
-        // account for the remaining prime factor greater than sqrt(n)
-        // E.g. if n=10, such factor would be 5
-        ++factors;
-    }
-
-    return factors;
+    // no factors: n is prime
+    num_factors.push_back(1);
+    if (n <= MAX_PRIME_TO_ADD) primes.push_back(n);
+    return 1;
 }
 
 int main()
 {
-    int ceil_sqrt_n = 1;
-    int ceil_sqrt_n_square = 1;
-    int odd_factored_numbers_found = 0; // consider 1 to have even number of prime factors (zero)
+    int floor_sqrt_n = 1;
+    int next_square = (floor_sqrt_n + 1) * (floor_sqrt_n + 1);
+    int max_prime_to_consider = 1;
+    int max_prime_idx_to_consider = -1;
 
-    for (int n = 2; n < 10000000000; ++n)
+    for (int n = 2; n <= TOTAL_NUMBERS; ++n)
     {
-        if (ceil_sqrt_n_square < n)
+        if (n >= next_square)
         {
-            ++ceil_sqrt_n;
-            ceil_sqrt_n_square = ceil_sqrt_n * ceil_sqrt_n;
+            ++floor_sqrt_n;
+            next_square = (floor_sqrt_n + 1) * (floor_sqrt_n + 1);
         }
 
-        auto factors = get_num_prime_factors(n, ceil_sqrt_n);
-        if (factors % 2 != 0)
+        if (max_prime_to_consider < floor_sqrt_n && next_square <= TOTAL_NUMBERS)
         {
-            ++odd_factored_numbers_found;
+            ++max_prime_idx_to_consider;
+            max_prime_to_consider = primes[max_prime_idx_to_consider];
         }
+
+        auto factors = calc_num_prime_factors(n, max_prime_idx_to_consider);
 
         // print progress
         if (n % 200000 == 0) cout << ".";
-        if (n % 10000000 == 0) cout << endl;
+        if (n % 10000000 == 0) cout << endl << n;
 
-        auto even_factored_numbers_found = n - odd_factored_numbers_found;
-        if (even_factored_numbers_found > odd_factored_numbers_found)
+        auto odd_factors = n - even_factors;
+        // cout << n << ":" << factors << ":" << odd_factors << ":" << even_factors << endl;
+
+        if (even_factors > odd_factors)
         {
-            cout << endl << n << ":" << even_factored_numbers_found-odd_factored_numbers_found << endl;
+            cout << endl << n << ":" << even_factors-odd_factors << endl;
         }
     }
+
+    for (auto prime : primes)
+        cout << prime << " ";
 }
 
 
